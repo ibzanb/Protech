90c143f15b6d8d85978340987bf6d1fc123fcc0a
Imac 2012 <imac2012@iMac-de-Imac.local>
2020-04-17 19:20:40 -0500

    ok

--- protech/index.php
+++ protech/index.php
@@ -66,7 +66,8 @@
                                 <br />
                                 <div>
                                     <h1><i class="fa fa-ticket"></i> Ticketly!</h1>
-                                    <p> <a style="text-decoration: underline;" target="_blank" href="https://github.com/ibzanb/Protech">Support</a>Sistema de tickets creado por Protech</p>
+                                    <p> <a style="text-decoration: underline;" target="_blank" href="https://github.com/ibzanb/Protech">Support</a>Sistema de tickets creado por Protech2.0</p>
+                                    
                                 </div>
                             </div>
                         </form>
